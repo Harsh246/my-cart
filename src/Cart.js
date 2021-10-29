73d2d7948b633f7cc77cebb15f49d0abd5cc0bab
Harsh246 <harsh0564.cse19@chitkara.edu.in>
2021-10-29 17:00:16 +0530

    used props in cart and cart items

--- src/Cart.js
+++ src/Cart.js
@@ -4,11 +4,49 @@ import CartItem from './CartItem';
 class Cart extends React.Component{
     render()
     {
+  
+        const property = [
+
+            {
+                price : 999,
+                qty: 1,
+                title: 'Mobile',
+                id:0
+            },
+            {
+                price : 99,
+                qty: 10,
+                title: 'Watch',
+                id:1
+            },
+            {
+                price : 9999,
+                qty: 4,
+                title: 'Laptop',
+                id:2
+            }
+
+
+            
+        ];
+
+
         return(
 <div className="cart"> 
-<CartItem />
-<CartItem />
-<CartItem />
+{
+property.map((index) => {
+
+return(
+<CartItem 
+properties={index}
+key={index.id}
+/>
+)
+
+})
+}
+
+
 </div>
         );
 
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 class CartItem extends React.Component {
     render() {
+        
+        const{ price , qty , title} = this.props.properties;
         return (
+
+
+
+
             <div className="cart-item" >
 
-                <div className="left-block">
+                <div >
                     <img style={{
                         height: 130,
                         width: 130,
@@ -14,25 +20,25 @@ class CartItem extends React.Component {
                     }} />
                 </div>
                 <div className="right-block">
-                    <div style={{ fontSize: 25 }}>MOBILE</div>
-                    <div style={{ color: '#777' }}>Rs 999 </div>
-                    <div style={{ color: '#777' }}>Qty: 01 </div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price} </div>
+                    <div style={{ color: '#777' }}>Qty: {qty }</div>
                     <div className="cart-item-actions">
                         <img
                             alt="increase"
                             className="action-icons"
-                            src="https://image.flaticon.com/icons/svg/992/992651.svg"
-                            onClick={this.increaseQuantity}
+                            src="https://www.svgrepo.com/show/71781/plus.svg"
+                         
                         />
                         <img
                             alt="decrease"
                             className="action-icons"
-                            src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0xMS41IDBjNi4zNDcgMCAxMS41IDUuMTUzIDExLjUgMTEuNXMtNS4xNTMgMTEuNS0xMS41IDExLjUtMTEuNS01LjE1My0xMS41LTExLjUgNS4xNTMtMTEuNSAxMS41LTExLjV6bTAgMWM1Ljc5NSAwIDEwLjUgNC43MDUgMTAuNSAxMC41cy00LjcwNSAxMC41LTEwLjUgMTAuNS0xMC41LTQuNzA1LTEwLjUtMTAuNSA0LjcwNS0xMC41IDEwLjUtMTAuNXptLTYuNSAxMGgxM3YxaC0xM3YtMXoiLz48L3N2Zz4="
+                            src="https://www.svgrepo.com/show/155829/minus.svg"
                         />
                         <img
                             alt="delete"
                             className="action-icons"
-                            src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+                            src="https://www.svgrepo.com/show/243868/delete.svg"
                         />
 
                     </div>

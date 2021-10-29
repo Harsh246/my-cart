b0b1d4fc3c4f834d59b4c31eca270b0365da68ad
Harsh246 <harsh0564.cse19@chitkara.edu.in>
2021-10-29 23:12:08 +0530

    Added functionalities to action icons +, - and delete

--- src/Cart.js
+++ src/Cart.js
@@ -1,53 +1,95 @@
 import React from 'react';
 import CartItem from './CartItem';
 
-class Cart extends React.Component{
-    render()
-    {
-  
-        const property = [
+class Cart extends React.Component {
+    constructor(){
+super();
+this.state = {
+    property: [
 
-            {
-                price : 999,
-                qty: 1,
-                title: 'Mobile',
-                id:0
-            },
-            {
-                price : 99,
-                qty: 10,
-                title: 'Watch',
-                id:1
-            },
-            {
-                price : 9999,
-                qty: 4,
-                title: 'Laptop',
-                id:2
+        {
+            price: 999,
+            qty: 1,
+            title: 'Mobile',
+            id: 0
+        },
+        {
+            price: 99,
+            qty: 10,
+            title: 'Watch',
+            id: 1
+        },
+        {
+            price: 9999,
+            qty: 4,
+            title: 'Laptop',
+            id: 2
+        }
+
+
+
+    ]
+}
+
+    }
+
+
+handleIncrease = (item)=>{
+     
+
+    const { property} = this.state;
+    const index = property.indexOf(item);
+
+    property[index].qty += 1;
+
+    this.setState({
+      property
+    })
+    }
+
+    handleDecrease = (item) => {
+const {property } = this.state;
+const index = property.indexOf(item);
+if(property[index].qty>0)
+property[index].qty -=1;
+
+this.setState({
+    property
+})
+
+    }
+
+    handleDelete = (id) => {
+        const {property} = this.state;
+        const items = property.filter( (item)=> item.id != id);
+        this.setState({
+            property: items
+        
+        })
+    } 
+    render() {
+const {property} = this.state;
+
+        return (<
+            div className="cart" > {
+                property.map((index) => {
+
+                    return (
+                        <CartItem
+                            properties={index}
+                            key={index.id}
+                            onIncrease = {this.handleIncrease}
+                            onDecrease  = {this.handleDecrease}
+                            onDelete =  {this.handleDelete}
+
+                        />
+                    )
+
+                })
             }
 
 
-            
-        ];
-
-
-        return(
-<div className="cart"> 
-{
-property.map((index) => {
-
-return(
-<CartItem 
-properties={index}
-key={index.id}
-/>
-)
-
-})
-}
-
-
-</div>
+        </div>
         );
 
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -2,15 +2,13 @@ import React from 'react';
 
 class CartItem extends React.Component {
     render() {
-        
+       // console.log('this.props', this.props);
         const{ price , qty , title} = this.props.properties;
-        return (
-
-
-
-
+       
+         //  console.log('this.props.onIncrease', this.props.onIncrease());
+ return (
             <div className="cart-item" >
-
+{this.props.jsx}
                 <div >
                     <img style={{
                         height: 130,
@@ -28,25 +26,25 @@ class CartItem extends React.Component {
                             alt="increase"
                             className="action-icons"
                             src="https://www.svgrepo.com/show/71781/plus.svg"
+                           onClick= { ()=> this.props.onIncrease(this.props.properties) }
                          
                         />
                         <img
                             alt="decrease"
                             className="action-icons"
                             src="https://www.svgrepo.com/show/155829/minus.svg"
+                            onClick= { ()=> this.props.onDecrease(this.props.properties) }
                         />
                         <img
                             alt="delete"
                             className="action-icons"
                             src="https://www.svgrepo.com/show/243868/delete.svg"
+                            onClick = {()=> this.props.onDelete(this.props.properties.id)}
                         />
 
                     </div>
 
                 </div>
-
-
-
 
             </div>
         );
